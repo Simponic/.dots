329c5adb2d3156f195c91703fe22fd14b84d6a3c
Simponic <loganhunt@simponic.xyz>
2022-11-07 15:47:39 -0700

    Add thefuck and comment out songnotification for now

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,7 +9,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(magit counsel doom-modeline doom-themes use-package))
+ '(package-selected-packages
+   '(kotlin-mode magit counsel doom-modeline doom-themes use-package))
  '(tab-width 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
