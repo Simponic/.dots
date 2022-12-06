df00aae35a9ceee82c5516e5eadea3379a0dadee
Logan Hunt <logan.hunt@usu.edu>
2022-12-05 17:13:06 -0700

    Add typescript mode tide and move ascii table to one hideous line

--- scripts/ascii-table.py
+++ scripts/ascii-table.py
@@ -1,5 +1,2 @@
 #!/usr/bin/python3
-
-rows = (128-32) // 4
-for i in range(rows):
-    print("".join(list(map(lambda x: str(x*rows+i+32).ljust(5) + str(chr(x*rows + i + 32)).strip().ljust(2) + "|", range(4)))))
+(lambda rows: print("\n".join(["".join(list(map(lambda x: str(x*rows+i+32).ljust(5) + str(chr(x*rows + i + 32)).strip().ljust(2) + "|", range(4)))) for i in range(rows)])))((128-32) // 4)
