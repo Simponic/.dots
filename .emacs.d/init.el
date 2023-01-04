710fd57524ab5a1d0ebffa28ab0dba81035d48d0
Simponic <loganhunt@simponic.xyz>
2023-01-04 00:06:20 -0700

    Add elcord options

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -30,6 +30,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(elcord-idle-message "Idling...")
+ '(elcord-idle-timer 120)
+ '(elcord-refresh-rate 3)
  '(package-selected-packages
    '(kotlin-mode magit counsel doom-modeline doom-themes use-package))
  '(tab-width 2))
