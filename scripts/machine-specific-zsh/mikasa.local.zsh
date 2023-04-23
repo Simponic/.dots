0de65823f59dc86c06fdb1246f1a160298beada5
Elizabeth Hunt <elizabeth.hunt@simponic.xyz>
2023-04-22 22:49:51 -0600

    Add postgres 15 to mikasa path

--- scripts/machine-specific-zsh/mikasa.local.zsh
+++ scripts/machine-specific-zsh/mikasa.local.zsh
@@ -6,6 +6,7 @@ export PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}";
 export MANPATH="/opt/homebrew/share/man${MANPATH+:$MANPATH}:";
 export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
 export PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"
+export PATH="/opt/homebrew/opt/postgresql@15/bin:$PATH"
 
 # asdf
 . /opt/homebrew/opt/asdf/libexec/asdf.sh
