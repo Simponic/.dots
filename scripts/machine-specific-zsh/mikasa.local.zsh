2b8ac635aeb1b434660846dbc90902b25a144d70
Elizabeth Hunt <elizabeth.hunt@simponic.xyz>
2023-04-22 23:50:48 -0600

    Add openjdk to path

--- scripts/machine-specific-zsh/mikasa.local.zsh
+++ scripts/machine-specific-zsh/mikasa.local.zsh
@@ -7,6 +7,7 @@ export MANPATH="/opt/homebrew/share/man${MANPATH+:$MANPATH}:";
 export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
 export PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"
 export PATH="/opt/homebrew/opt/postgresql@15/bin:$PATH"
+export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
 
 # asdf
 . /opt/homebrew/opt/asdf/libexec/asdf.sh
