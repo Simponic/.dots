d08519918c36f9aed1c7a93c0920c0b8b4956c35
Lizzy Hunt <lizzy.hunt@usu.edu>
2023-05-02 11:07:53 -0600

    Forgot executable bit rip

--- scripts/install_emacs.sh
+++ scripts/install_emacs.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 [ -z "$EMACS_VERSION" ] && echo "Usage: EMACS_VERSION=25.1 install-emacs.sh or EMACS_VERSION=snapshot install-emacs.sh" && exit 1
 [ -z "$EMACS_URL" ] && EMACS_URL="http://mirror.aarnet.edu.au/pub/gnu/emacs/"
-# I've assign 12G memory to /tmp as ramdisk
 [ -z "$EMACS_TMP" ] && EMACS_TMP="/tmp"
 
 if [ "$EMACS_VERSION" != "snapshot" ]; then
@@ -9,9 +8,6 @@ if [ "$EMACS_VERSION" != "snapshot" ]; then
     curl $EMACS_URL/emacs-$EMACS_VERSION.tar.gz | tar xvz -C $EMACS_TMP
 fi
 
-# @see http://wiki.gentoo.org/wiki/Project:Emacs/GNU_Emacs_developer_guide
-# @see http://packages.gentoo.org/package/app-editors/emacs for info on Gentoo Linux
-# --without-gtk and --without-gtk3 is optional
 echo "Installing Emacs ..."
 if [ "$EMACS_VERSION" = "snapshot" ]; then
     cd $HOME/projs/emacs && mkdir -p $HOME/myemacs/snapshot && rm -rf $HOME/myemacs/snapshot/* && ./autogen.sh && ./configure CFLAGS=-no-pie --prefix=$HOME/myemacs/snapshot --without-x --without-dbus --without-sound --with-gnutls=no && make -j6 && make install
