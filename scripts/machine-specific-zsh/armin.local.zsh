5566489848c92ace079769fa96c47c8f6da86a3d
Elizabeth Hunt <elizabeth.hunt@simponic.xyz>
2023-05-27 17:29:07 -0700

    move config from old m1 to new

--- scripts/machine-specific-zsh/armin.local.zsh
+++ scripts/machine-specific-zsh/armin.local.zsh
@@ -2,12 +2,17 @@
 export HOMEBREW_PREFIX="/opt/homebrew";
 export HOMEBREW_CELLAR="/opt/homebrew/Cellar";
 export HOMEBREW_REPOSITORY="/opt/homebrew";
-export PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}";
+export PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"
+
 export MANPATH="/opt/homebrew/share/man${MANPATH+:$MANPATH}:";
 export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
-export PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"
 export PATH="/opt/homebrew/opt/postgresql@15/bin:$PATH"
 export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
+export PATH=$PATH:/Users/lizzy/.spicetify
+export PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}";
 
 # asdf
 . /opt/homebrew/opt/asdf/libexec/asdf.sh
+
+
+export PATH=$PATH:/Users/lizzy/.spicetify
