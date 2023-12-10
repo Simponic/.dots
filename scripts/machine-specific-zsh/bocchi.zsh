ce4ea88467bb334c2a4d8b3e0d97542ac8f6ba5b
Elizabeth Hunt <elizabeth.hunt@simponic.xyz>
2023-12-09 19:06:26 -0700

    update emacs font and whatnot

---
+++ scripts/machine-specific-zsh/bocchi.zsh
@@ -0,0 +1,10 @@
+export PATH=$PATH:/home/lizzy/myemacs/28.1/bin
+export PATH=$PATH:/home/lizzy/.cargo/bin
+export PATH=$PATH:/home/lizzy/.local/bin
+export PATH=$PATH:/home/lizzy/.roswell/bin
+
+source /etc/profile.d/devkit-env.sh
+
+alias emacs="emacsclient -nw"
+
+. "$HOME/.asdf/asdf.sh"
